70edad42eae5f5391c26985586c090ae00515513
AJSchneida <AJSchneida@users.noreply.github.com>
2014-07-17 01:34:47 -0400

    Update Player.java
    playerStats implemented, new Stats object created by init

--- src/textbasedengine/entities/Player.java
+++ src/textbasedengine/entities/Player.java
@@ -2,24 +2,35 @@ package textbasedengine.entities;
 
 import textbasedengine.Main;
 
+/**
+ * Handles the logic and data for the player
+ * @author Austin
+ * @version 0.0.1
+ */
 public class Player {
 	public static Location currentLocation;
 	public static boolean dead = false;
+	public static Stats playerStats;
 	
+	/**
+	 * Resets the output window and initializes player information
+	 * 
+	 */
 	public static void init() {
 		dead = false;
 		Main.output.setText("");
 		moveTo(Location.start());
+		playerStats = new Stats("mainCharacter", 100, 100);
 	}
 	
+	/**
+	 * Moves the player to a new location, displaying its description
+	 * @param newLoc The location to move to
+	 */
 	public static void moveTo(Location newLoc) {
 		currentLocation = newLoc;
 		Main.output.append(String.format(newLoc.getDescription() + "%n"));
 		newLoc.onEnter();
 	}
 	
-	public static void death() {
-		dead = true;
-		Main.output.append(String.format("You are dead. Type \"start over\" to restart.%n"));
-	}
 }
