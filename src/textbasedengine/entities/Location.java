d04ae64f7d2a953dc568d7d9e01c83d07de03d4d
AJSchneida <AJSchneida@users.noreply.github.com>
2014-07-17 01:34:06 -0400

    Update Location.java
    changed case "DEATH" in handleEvents to call new setHealth method

--- src/textbasedengine/entities/Location.java
+++ src/textbasedengine/entities/Location.java
@@ -1,18 +1,29 @@
 package textbasedengine.entities;
 
 import java.io.File;
-
+import textbasedengine.entities.Player;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
+/**
+ * The representation of a position the player can move to. Contains information on the phrases that move the player to another room
+ * @author Austin
+ * @version 0.0.1
+ */
 public class Location {
 	private static Location[] all;
 	
+	/**
+	 * @return The first room for the player to start in
+	 */
 	public static Location start() { return all[0]; }
 	
+	/**
+	 * Initializes the location data from the stored text file
+	 */
 	public static void Load() {
 		File xml = new File("src/textbasedengine/entities/LocationData.xml");
 		try {
@@ -60,11 +71,16 @@ public class Location {
 		}
 	}
 	
+	/**
+	 * Handles an array of events for a location
+	 * @param events The events to handle
+	 */
+
 	public static void handleEvents(String[] events) {
 		for (String event : events)
 			switch (event.toUpperCase()) {
 				case "DEATH":
-					Player.death();
+					Stats.setHealth(0);
 					break;
 			}
 	}
@@ -75,6 +91,13 @@ public class Location {
 	private boolean[] open;
 	private String[] enterEvents;
 	
+	/**
+	 * @param desc The description given on the room being entered or looked at
+	 * @param phrases The phrases that connect adjacent locations
+	 * @param connections Ids of connected locations
+	 * @param open Whether an adjacent location is open to be moved to
+	 * @param enterEvents The events that occur when this room is entered
+	 */
 	public Location(String desc, String[] phrases, int[] connections, boolean[] open, String[] enterEvents) {
 		this.desc = desc;
 		this.phrases = phrases;
@@ -83,11 +106,19 @@ public class Location {
 		this.enterEvents = enterEvents;
 	}
 	
+	/**
+	 * @return The description for this room
+	 */
 	public String getDescription() { return desc; }
 	
+	/**
+	 * Determines if a given command matches any adjacent location phrases
+	 * @param phrase The given command
+	 * @return Whether there was a match found
+	 */
 	public boolean checkPhrase(String phrase) {
 		for (int i = 0; i < phrases.length; i++)
-			if (phrases[i].equals(phrase)) {
+			if (phrases[i].equals(phrase) && open[i]) {
 				Player.moveTo(all[connections[i]]);
 				return true;
 			}
@@ -95,5 +126,8 @@ public class Location {
 		return false;
 	}
 	
+	/**
+	 * Called when a location is entered, runs all onEnter events
+	 */
 	public void onEnter() { handleEvents(enterEvents); }
 }
